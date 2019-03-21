a11bf97629c31f72381c48127119401d8068da98
Christian Cadruvi <cadruvi@mediamarktsaturn.com>
2019-03-21 12:12:40 +0000

    Add credentials getting for alias

--- bash/aliases/kubectl.sh
+++ bash/aliases/kubectl.sh
@@ -4,8 +4,14 @@ then
 	alias top='htop'
 fi
 
+sp(){
+	gcloud config set project "$1"
+	gcloud container clusters get-credentials cluster
+}
+
 kc(){
 	echo "Namespace set to $1"
+	gcloud container clusters get-credentials cluster -z europe-west4-b
 	kubectl config set-context $(gcloud container clusters list --format="value(name)") --namespace "$1"
 	kubectl config set-context $(kubectl config current-context) --namespace="$1"
 }
