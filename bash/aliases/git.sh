4bec39c67d3e06c807760d7413545efdd703a462
ccadruvi <git@n5.at>
2022-01-24 09:17:01 +0100

    Add branch deletion and listing

--- bash/aliases/git.sh
+++ bash/aliases/git.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#set -euo pipefail
 
 alias gs='git status'
 alias gd='git diff'
@@ -13,4 +14,30 @@ function gc() {
 }
 function ga() {
   git add $@
+}
+
+function gb() {
+  # Only output color if the command isn't being piped.
+  if [ -t 1 ]; then
+    COLOR="always"
+  else
+    COLOR="auto"
+  fi
+  
+  git branch \
+    --all \
+    --color="$COLOR" \
+    --sort=authordate \
+    --format="%(color:blue)%(authordate:relative);%(color:red)%(authorname);%(color:white)%(color:bold)%(refname:short)" \
+    "$@" \
+    | column -s ";" -t
+}
+
+function gbd() {
+  git checkout master; 
+  git fetch --prune; 
+  branchesToDelete=$(git branch -vv | grep ': gone' | awk '{print $1}')
+  for branch in $branchesToDelete; do
+    git branch -D "$branch"
+  done
 }
