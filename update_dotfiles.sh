832934a57e50f7be84311eed74ac7b6c74bf1448
Christian Cadruvi <christian.cadruvi@digitecgalaxus.ch>
2024-07-01 15:39:20 +0200

    Add shortcuts with KDE

--- update_dotfiles.sh
+++ update_dotfiles.sh
@@ -5,5 +5,6 @@ rm -rf "$HOME/.bash"
 cp -r "$DOTFILES_DIR/bash" "$HOME/.bash"
 cp -r "$DOTFILES_DIR/bashrc" "$HOME/.bashrc"
 cp "$DOTFILES_DIR/vimrc" "$HOME/.vimrc"
+cp "$DOTFILES_DIR/khotkeysrc" "$HOME/.config/khotkeysrc"
 source "$HOME/.bashrc"
 
